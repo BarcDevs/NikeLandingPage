543505d4bc26cf29f8039aed85f66dd3e90f7a54
Bar Cohen <sscohen99@gmail.com>
2023-10-15 20:10:53 +0300

    app structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-const App = ({}) => {
-    return (
-        <div>
-          
-        </div>
-    )
-}
+const App = ({}) => (
+    <main className={'relative'}>
+        Nav
+
+        <section className="xl:padding-1 wide:padding-r padding-b">
+            Hero
+        </section>
+
+        <section className="padding">
+            PopularProducts
+        </section>
+
+        <section className="padding">
+            SuperQuality
+        </section>
+
+        <section className="padding-x py-10">
+            Services
+        </section>
+
+        <section className="padding">
+            SpecialOffer
+        </section>
+
+        <section className="padding bg-pale-blue">
+            CustomerReviews
+        </section>
+
+        <section className="padding-x w-full py-16 sm:py-32">
+            Subscribe
+        </section>
+
+        <footer className="padding-x padding-t bg-black pb-8">
+            Footer
+        </footer>
+
+    </main>
+)
+
 
 export default App
