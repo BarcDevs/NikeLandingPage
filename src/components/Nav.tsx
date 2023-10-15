0a5411453c4152bf1c0271e98cd801e7626ec9df
Bar Cohen <sscohen99@gmail.com>
2023-10-15 21:20:37 +0300

    added navbar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,47 @@
+// @ts-ignore
+import {headerLogo} from '../assets/images'
+// @ts-ignore
+import {hamburger} from '../assets/icons'
+import {navLinks} from '../constants'
+
+const Nav = ({}) => {
+    return (
+        <header className={'padding-x absolute z-10 w-full py-8'}>
+            <nav className={'max-container flex items-center justify-between'}>
+                <a href="/">
+                    <img
+                        src={headerLogo}
+                        alt="logo"
+                        height={29}
+                        width={130}
+                    />
+                </a>
+
+                <ul className="flx-1 flex items-center justify-center gap-16 max-lg:hidden">
+                    {navLinks.map(item => (
+                        <li key={item.label}>
+                            <a
+                                href={item.href}
+                                className={'font-montserrat text-lg leading-normal text-slate-gray'}
+                            >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+
+                {/*TODO: add functionality*/}
+                <div className={'lg:hidden'}>
+                    <img
+                        src={hamburger}
+                        alt="menu"
+                        width={25}
+                        height={25}
+                    />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
