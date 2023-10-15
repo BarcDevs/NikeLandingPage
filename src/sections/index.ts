d270b257cd34c7d4560ebb2e82f79f5b95559aa0
Bar Cohen <sscohen99@gmail.com>
2023-10-15 20:23:07 +0300

    added section components placeholders

---
+++ src/sections/index.ts
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews.tsx'
+import Hero from './Hero.tsx'
+import PopularProducts from './PopularProducts.tsx'
+import Services from './Services.tsx'
+import SpecialOffer from './SpecialOffer.tsx'
+import Subscribe from './Subscribe.tsx'
+import SuperQuality from './SuperQuality.tsx'
+import Footer from './Footer.tsx'
+
+export {
+    CustomerReviews,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
