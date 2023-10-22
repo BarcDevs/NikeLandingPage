7e3f58b75b1b916953adff20895f09b9bcad36f0
Bar Cohen <sscohen99@gmail.com>
2023-10-22 21:00:12 +0300

    lint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'prettier',
-    'plugin:tailwindcss/recommended'
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', '@typescript-eslint'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'prettier',
+        'plugin:tailwindcss/recommended'
     ],
-    "react/prop-types": 0,
-    'no-empty-pattern': 0,
-    'tailwindcss/no-custom-classname': 0,
-  },
-  parser: '@typescript-eslint/parser'
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+    settings: {react: {version: '18.2'}},
+    plugins: ['react-refresh', '@typescript-eslint'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true}
+        ],
+        "react/prop-types": 0,
+        'no-empty-pattern': 0,
+        'tailwindcss/no-custom-classname': 0,
+        'no-unused-vars': 'error',
+        '@typescript-eslint/no-unused-vars': 'error'
+    },
+    parser: '@typescript-eslint/parser'
 }
